41da0ed97cbedd76a20950c67cf1334fe34b0352
Brian-mula <mulatiwangilabrian@gmail.com>
2023-10-01 17:39:29 +0300

    modified nav bar

--- src/components/nav/Navbar.tsx
+++ src/components/nav/Navbar.tsx
@@ -1,13 +1,19 @@
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
     <div className="navbar bg-base-100">
   <div className="flex-1">
-    <a className="btn btn-ghost text-xl uppercase">worldwise</a>
+    <Link to="" className="btn btn-ghost text-xl uppercase">worldwise</Link>
   </div>
   <div className="flex-none">
     <ul className="menu menu-horizontal px-1">
-      <li><a>Link</a></li>
+      <li>
+        <Link to="/products" className="">Products</Link>
+      </li>
+      <li>
+        <Link to="/pricing" className="">Pricing</Link>
+      </li>
       <li>
         <details>
           <summary>
