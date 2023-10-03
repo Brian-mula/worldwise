3711f2f1d069df97780d2e2de746d1997c4aec42
Brian-mula <mulatiwangilabrian@gmail.com>
2023-10-03 14:02:52 +0300

    added app layout

--- src/pages/AppLayout.tsx
+++ src/pages/AppLayout.tsx
@@ -1,9 +1,9 @@
-import { Link, Outlet, useNavigate } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 import MapDisplay from "../components/map/MapDisplay";
 import AppNav from "../components/nested/AppNav";
 
 export default function AppLayout() {
-  const navigate = useNavigate();
+  
   return (
     <div className="flex h-screen ">
         <div className="w-1/3 bg-gray-800 py-3">
@@ -14,7 +14,7 @@ export default function AppLayout() {
         <Outlet/>
         </div>  
 
-        <div className="w-2/3 px-2 h-screen bg-gray-300" onClick={()=>navigate('form')} >
+        <div className="w-2/3 px-2 h-screen bg-gray-300"  >
           <MapDisplay/>
         </div>
     </div>
