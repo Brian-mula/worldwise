7b7d30b5c83c4d1eb652b5e46aa75a10a84a12fc
Brian-mula <mulatiwangilabrian@gmail.com>
2023-10-03 14:02:13 +0300

    modified map display

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import { useEffect, useState } from "react";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { CitiesProvider } from "./contexts/CitiesContext";
 import AppLayout from "./pages/AppLayout";
 import Cities from "./pages/Cities";
 import CityDetails from "./pages/CityDetails";
@@ -11,28 +11,11 @@ import Pricing from "./pages/Pricing";
 import Products from "./pages/Products";
 
 function App() {
-  const [cities, setCities] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-
-  useEffect(() => {
-    async function fetchCities() {
-      try {
-        setIsLoading(true);
-      const response = await fetch("http://localhost:3001/cities");
-      const json = await response.json();
-      setCities(json);
-      
-      } catch (error) {
-        alert("Error fetching cities");
-      }finally{
-        setIsLoading(false);
-      }
-    }
-    fetchCities();
-  }, []);
+ 
   return (
-   <div>
-    
+   
+    <CitiesProvider>
+
     <BrowserRouter>
     
    <Routes>
@@ -41,16 +24,17 @@ function App() {
     <Route path="pricing" element={<Pricing />} />
     <Route path="app" element={<AppLayout/>} >
       <Route index element={<Navigate to="cities" />} />
-      <Route path="cities" element={<Cities cities={cities} isLoading={isLoading}/>} />
+      <Route path="cities" element={<Cities />} />
       <Route path="cities/:id" element={<CityDetails />} />
-      <Route path="countries" element={<Countires cities={cities} />} />
+      <Route path="countries" element={<Countires />} />
       <Route path="form" element={<Form/>} />
        </Route>
     <Route path="*" element={<NotFoundPage/>} />
 
    </Routes>
    </BrowserRouter>
-   </div>
+    </CitiesProvider>
+  
   );
 }
 
