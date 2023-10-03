04231f3f61d12ecb7ac919b795c78128b733015c
Brian-mula <mulatiwangilabrian@gmail.com>
2023-10-03 14:02:29 +0300

    modified map display

--- src/components/map/MapDisplay.tsx
+++ src/components/map/MapDisplay.tsx
@@ -1,12 +1,59 @@
-import { useSearchParams } from "react-router-dom";
+import { LatLngExpression } from "leaflet";
+import { useEffect, useState } from "react";
+import { MapContainer, Marker, Popup, TileLayer, useMap, useMapEvents } from "react-leaflet";
+import { useNavigate, useSearchParams } from "react-router-dom";
+import { useCities } from "../../contexts/CitiesContext";
 
 export default function MapDisplay() {
+  const [mapPosition,setMapPosition] = useState<LatLngExpression>([51.505, -0.09]);
+
+  
+
+  const {cities} = useCities();
     const[search] = useSearchParams()
-    const lat = search.get('lat');
-    const lng = search.get('lng');
+    const mapLat = search.get('lat');
+    const mapLng = search.get('lng');
+
+    useEffect(() => {
+      if(mapLat && mapLng){
+        setMapPosition([Number(mapLat), Number(mapLng)])
+      }
+    },[mapLat, mapLng])
+    // if(mapLat && mapLng){
+    //   setMapPosition([Number(mapLat), Number(mapLng)])
+    // }
   return (
-    <div className=" ">
-        <h1>MapDisplay: {lat} {lng}</h1>
+    <div className="w-full h-full ">
+        <MapContainer center={mapPosition} zoom={6} scrollWheelZoom={true} className="h-full">
+    <TileLayer
+      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    />
+    {
+        cities.map((city)=>(
+          <Marker position={[city.position.lat,city.position.lng]} key={city.id} >
+          <Popup>
+            <span>{city.emoji}</span> <span>{city.name}</span>
+          </Popup>
+        </Marker>
+        ))
+    }
+    <ChangeMapCenter position={[Number(mapLat),Number(mapLng)]} />
+    <DetectClick />
+  </MapContainer>
     </div>
   )
+}
+function ChangeMapCenter({position}:{position: [number, number]}){
+  const map = useMap();
+  map.setView(position, 6);
+  return null;
+}
+
+function DetectClick(){
+  const navigate = useNavigate();
+  useMapEvents({
+    click: (e)=>navigate(`form?lat=${e.latlng.lat}&lng=${e.latlng.lng}`)
+  })
+  return null;
 }
